4d1b3dfd240350da937a24a1f87b7db06b1ff0e4
Victor H Correa <Viktorhugo.dev@gmail.com>
2022-06-28 09:16:20 -0300

    fix colors

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -9,8 +9,7 @@ export function Subscribe() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [createSubscriber, { data, loading }] = useCreateSubscriberMutation()
-  console.log('data', data)
-  console.log('loading', loading)
+
   async function handleSubscribe(event: FormEvent) {
     event.preventDefault()
     console.log('passo 1')
@@ -25,24 +24,25 @@ export function Subscribe() {
     console.log(data)
   }
   return (
-    <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
-      <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
-        <div className='max-w-[640px]'>
+    <div className="backdrop-blur-md backdrop-brightness-150 bg-center bg-cover bg-no-repeat bg-blur ">
+    <div className='min-h-screen  flex flex-col items-center backdrop-blur-sm backdrop-brightness-100'>
+      <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-autox'>
+        <div className='max-w-[640px] '>
           <Logo />
           <h1 className='mt-8 text-[2.5rem] leading-tight'>
             Construa uma{' '}
-            <strong className='text-blue-500'>aplicação completa</strong>, do
-            zero, com <strong className='text-blue-500'>React JS</strong>
+            <strong className='text-purple-300'>aplicação completa</strong>, do
+            zero, com <strong className='text-purple-300'>React JS</strong>
           </h1>
-          <p className='mt-4 text-gray-200 leading-relaxed'>
+          <p className='mt-4 text-gray-200 leading-relaxed '>
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
-
-        <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
-          <strong className='text-2xl mb-6 block'>
+        
+        <div className='p-8 dark:bg-gray-700 border-2 border-gray-500 rounded'>
+          <strong className='text-2xl mb-6 text-gray-300 block'>
             Inscreva-se gratuitamente
           </strong>
 
@@ -51,13 +51,13 @@ export function Subscribe() {
             className='w-full flex flex-col gap-2'
           >
             <input
-              className='bg-gray-900 rounded px-5 h-14'
+              className='bg-purple-200 dark:bg-gray-900 rounded px-5 h-14'
               type='text'
               placeholder='Digite seu nome completo'
               onChange={event => setName(event.target.value)}
             />
             <input
-              className='bg-gray-900 rounded px-5 h-14'
+              className='bg-purple-200 dark:bg-gray-900  rounded px-5 h-14'
               type='email'
               placeholder='Digite seu email'
               onChange={event => setEmail(event.target.value)}
@@ -65,7 +65,7 @@ export function Subscribe() {
             <button
               type='submit'
               disabled={loading}
-              className='mt-4 bg-green-500 uppercase py-4 font-bold text-sm hover:bg-green-700 transition-colors
+              className='mt-4 bg-purple-500 uppercase py-4 font-bold text-sm hover:bg-purple-700 transition-colors
           w-full disabled:opacity-50
           '
             >
@@ -76,6 +76,7 @@ export function Subscribe() {
       </div>
 
       <div className=' min-h-[500px] bg-screen w-[1000px] bg-cover bg-no-repeat mx-auto'></div>
+    </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        blur: 'url(/src/assets/background.png)',
-        screen:'url(/src/assets/back-2.png)'
+        blur: 'url(https://images.unsplash.com/photo-1567095761054-7a02e69e5c43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)',
+        screen:'url(/src/assets/back-2.png)',
+   
       },
       fontFamily: {
         sans: 'Roboto, sans-serif',
